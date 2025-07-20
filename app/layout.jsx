86e15cb7b7da8dc2e1c6d62fcbece9fd68324cf0
Md. Masum Musfique <musfiquemasum8@gmail.com>
2025-07-21 00:33:50 +0600

    style updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,11 +5,17 @@ import '../styles/globals.css'
 
 export const metadata = {
   title: 'Debate Arena',
+  icons: {
+    icon: "/icons8-vote-100.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
+      {/* <head>
+        <link rel="icon" href="/icons8-vote-100.png" type="image/png" />
+      </head> */}
       <body className="min-h-screen bg-base-100">
         <ClientLayout>{children}</ClientLayout>
       </body>
