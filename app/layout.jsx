b383476fc24b0401970b208f7bd490026d2bdd3d
Md. Masum Musfique <musfiquemasum8@gmail.com>
2025-07-21 00:42:04 +0600

    icon added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,20 +2,16 @@ import '../styles/globals.css';
 import ClientLayout from './ClientLayout';
 import '../styles/globals.css'
 
-
 export const metadata = {
   title: 'Debate Arena',
   icons: {
-    icon: "/icons8-vote-100.png",
+    icon: '/assets/icons8-vote-100.png',
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      {/* <head>
-        <link rel="icon" href="/icons8-vote-100.png" type="image/png" />
-      </head> */}
       <body className="min-h-screen bg-base-100">
         <ClientLayout>{children}</ClientLayout>
       </body>
