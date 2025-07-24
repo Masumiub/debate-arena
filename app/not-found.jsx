b273a75236f0b720f6ce0bdc0acd18ddc8e94292
Md. Masum Musfique <musfiquemasum8@gmail.com>
2025-07-25 00:48:36 +0600

    updated

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,5 +1,5 @@
+'use client';
 import Link from 'next/link';
-import React from 'react';
 import Lottie from "lottie-react";
 import ErrorImg from '../public/assets/Website maintenance, website problems, 404.json'
 
